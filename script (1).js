e5c3334d18baa19557023641af278c2693301086
allrob4329 <44777626+allrob4329@users.noreply.github.com>
2018-11-07 09:55:15 -0500

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,12 @@
+var penColour ='black';
+var penColour = 'white';
+
+function setPenColour(pen)
+{
+	penColour = pen;
+}
+function setPixelColour(pixel)
+{
+	pixel.style.backgroundColor = penColour;
+}
+
